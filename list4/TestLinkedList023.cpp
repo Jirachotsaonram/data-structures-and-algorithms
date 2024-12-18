89a37140dc3337b28513d889a34bc10d8e79aabd
Jirachot Saonram <109726423+Jirachotsaonram@users.noreply.github.com>
2024-12-18 11:37:05 +0700

    add

---
+++ list4/TestLinkedList023.cpp
@@ -0,0 +1,45 @@
+#include "LinkedList.h"
+#include "LinkedList.cpp"
+
+#include <iostream>
+using namespace std;
+
+// Simple print method
+template <class Object>
+void printList(const List<Object> &theList)
+{
+    if (theList.isEmpty())
+        cout << "Empty list" << endl;
+    else
+    {
+        ListItr<Object> itr = theList.first();
+        for (; !itr.isPastEnd(); itr.advance())
+            cout << itr.retrieve() << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    cout << "Jirachot Saonram ID:6706021411288 = 4 1 1 2 8 8" << endl;
+    List<int> a;
+    ListItr<int> b;
+    b = a.zeroth();
+    a.insert(4,b);
+    b.advance();
+    a.insert(1,b);
+    b = a.zeroth();
+    b.advance();
+    a.insert(1,b);
+    b = a.zeroth();
+    b.advance();
+    a.insert(2,b);
+    b = a.zeroth();
+    a.insert(8,b);
+    b = a.zeroth();
+    a.insert(8,b);
+    cout << "Sort from most to least";
+    printList(a);
+    return 0;
+}
